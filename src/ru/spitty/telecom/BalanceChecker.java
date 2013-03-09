281b34ac47f1add242e664f4c156e396388ffbcc
spitty <spitty.pnz@gmail.com>
2013-03-10 02:27:50 +0400

    Add case with incorrect login/password

--- src/ru/spitty/telecom/BalanceChecker.java
+++ src/ru/spitty/telecom/BalanceChecker.java
@@ -5,26 +5,31 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The core of balance checking. It uses {@link Jsoup} class (from <a href="http://jsoup.org/">Jsoup</a> library) for obtaining
- * the result of POST request to <a href="http://cabinet.telecom.mipt.ru/">http://cabinet.telecom.mipt.ru/</a> 
+ * The core of balance checking. It uses {@link Jsoup} class (from <a
+ * href="http://jsoup.org/">Jsoup</a> library) for obtaining the result of POST
+ * request to <a
+ * href="http://cabinet.telecom.mipt.ru/">http://cabinet.telecom.mipt.ru/</a>
  * and parsing.
- * 
+ *
  * @author spitty
  */
 public class BalanceChecker {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BalanceChecker.class);
     private static final String ENTER_URL = "http://cabinet.telecom.mipt.ru/";
-    
     private String login;
     private String password;
 
     /**
-     * Public constructor store <code>login</code> and <code>password</code>
+     * Public constructor store
+     * <code>login</code> and
+     * <code>password</code>
+     *
      * @param login {@link String} user login
      * @param password {@link String} user password
      */
@@ -32,13 +37,15 @@ public class BalanceChecker {
         this.login = login;
         this.password = password;
     }
-    
+
     /**
-     * Authorizes with given credentials on <a href="http://cabinet.telecom.mipt.ru/">http://cabinet.telecom.mipt.ru/</a>
+     * Authorizes with given credentials on <a
+     * href="http://cabinet.telecom.mipt.ru/">http://cabinet.telecom.mipt.ru/</a>
      * and returns a {@link Double} value of current balance.
+     *
      * @return
      * @throws IOException
-     * @throws NumberFormatException 
+     * @throws NumberFormatException
      */
     public Double getBalance() throws IOException, NumberFormatException {
         Connection.Response res = Jsoup
@@ -47,10 +54,15 @@ public class BalanceChecker {
                 .method(Connection.Method.POST)
                 .execute();
         Document doc = res.parse();
-        Element table = doc.getElementsByClass("tab").first();
-        Element balance = table.getElementsByTag("tr").get(1).getElementsByTag("td").get(1).child(0);
+        Elements error = doc.select("div#error");
+        if(!error.isEmpty()) {
+            throw new IllegalArgumentException("Login and password mismatch");
+        }
+        // Get element <table class="tab">. It is the only one such element on page
+        Element tableTag = doc.getElementsByClass("tab").first();
+        Element balanceTag = tableTag.select("tr > td > span").first();
         // Now we have string like "100.00 руб"
-        String balanceStr = balance.text().replaceFirst("^([0-9.]+).*", "$1");
+        String balanceStr = balanceTag.text().replaceFirst("^([0-9.]+).*", "$1");
         final Double doubleValue = Double.valueOf(balanceStr);
         return doubleValue;
     }
